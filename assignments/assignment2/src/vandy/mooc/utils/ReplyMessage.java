cfda1802d41f2558e54c001ce741197509fea522
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2015-05-13 11:46:20 -0500

    Updates.

--- assignments/assignment2/src/vandy/mooc/utils/ReplyMessage.java
+++ assignments/assignment2/src/vandy/mooc/utils/ReplyMessage.java
@@ -40,37 +40,25 @@ public class ReplyMessage extends RequestReplyMessageBase {
 
         // Create a new Bundle to handle the result.
         // TODO -- you fill in here.
-        Bundle data = new Bundle();
 
         // Put the URL to the image file into the Bundle via the
         // IMAGE_URL key.
         // TODO -- you fill in here.
-        data.putString(IMAGE_URL,
-                       url.toString());
 
         // Put the requestCode into the Bundle via the REQUEST_CODE
         // key.
         // TODO -- you fill in here.
-        data.putInt(REQUEST_CODE,
-                    requestCode);
 
         // Set a field in the Message to indicate whether the download
         // succeeded or failed.
         // TODO -- you fill in here.
-        replyMessage.mMessage.arg1 = pathToImageFile == null 
-            ? Activity.RESULT_CANCELED 
-            : Activity.RESULT_OK;
 
         // Put the path to the image file into the Bundle via the
         // IMAGE_PATHNAME key only if the download succeeded.
         // TODO -- you fill in here.
-        if (pathToImageFile != null)
-            data.putString(IMAGE_PATHNAME, 
-                           pathToImageFile.toString());
 
         // Set the Bundle to be the data in the message.
         // TODO -- you fill in here.
-        replyMessage.mMessage.setData(data);
 
         return replyMessage;
     }
