3dbcf8ae324d00fa16b22c87e078d52c1881b976
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2015-06-10 10:16:38 -0500

    Updates.

--- ex/AcronymApplicationRetained/src/vandy/mooc/utils/ExecutorServiceTimeoutCache.java
+++ ex/AcronymApplicationRetained/src/vandy/mooc/utils/ExecutorServiceTimeoutCache.java
@@ -176,6 +176,6 @@ public class ExecutorServiceTimeoutCache<K, V>
      */
     @Override
     protected void close() {
-        mScheduledExecutorService.shutdown();
+        mScheduledExecutorService.shutdownNow();
     }
 }
