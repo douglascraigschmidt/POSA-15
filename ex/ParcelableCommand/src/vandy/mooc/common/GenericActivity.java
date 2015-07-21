c6c51426633c0c8962d13a146fcbdf358d4543eb
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2015-07-21 11:33:52 -0500

    Updates.

--- ex/ParcelableCommand/src/vandy/mooc/common/GenericActivity.java
+++ ex/ParcelableCommand/src/vandy/mooc/common/GenericActivity.java
@@ -4,22 +4,34 @@ import android.content.Context;
 import android.util.Log;
 
 /**
- * This Activity provides a framework for mediating access to the
- * Presenter layer in the Model-View-Presenter pattern.  It
- * automatically handles runtime configuration changes in conjunction
- * with an instance of OpsType, which must implement the PresenterOps
- * interface.  It extends LifecycleLoggingActivity so that all
- * lifecycle hook method calls are automatically logged.  It also
- * implements the ContextView interface that provides access to the
- * Activity and Application contexts.
+ * This Activity provides a framework for mediating access to a object
+ * residing in the Presenter layer in the Model-View-Presenter (MVP)
+ * pattern.  It automatically handles runtime configuration changes in
+ * conjunction with an instance of PresenterType, which must implement
+ * the PresenterOps interface.  It extends LifecycleLoggingActivity so
+ * that all lifecycle hook method calls are automatically logged.  It
+ * also implements the ContextView interface that provides access to
+ * the Activity and Application contexts in the View layer.
+ *
+ * The three types used by a GenericActivity are the following:
+ * <ol>
+ *     <li><code>RequiredViewOps</code>, the class or interface that
+ *     defines the methods available to the Presenter object from the
+ *     View layer.</li> 
+ *     <li><code>ProvidedPresenterOps</code>, the class or interface
+ *     that defines the methods available to the View layer from the
+ *     Presenter object.</li>
+ *     <li><code>PresenterType</code>, the class created/used by the
+ *     GenericActivity framework to implement an Presenter object.</li>
+ * </ol>
  */
 public abstract class GenericActivity<RequiredViewOps,
-                                      ProvidedViewOps,
-                                      OpsType extends PresenterOps<RequiredViewOps>>
+                                      ProvidedPresenterOps,
+                                      PresenterType extends PresenterOps<RequiredViewOps>>
        extends LifecycleLoggingActivity
        implements ContextView {
     /**
-     * Used to retain the ProvidedViewOps state between runtime
+     * Used to retain the ProvidedPresenterOps state between runtime
      * configuration changes.
      */
     private final RetainedFragmentManager mRetainedFragmentManager 
@@ -27,21 +39,20 @@ public abstract class GenericActivity<RequiredViewOps,
                                       TAG);
  
     /**
-     * Instance of the operations ("Ops") type.
+     * Instance of the Presenter type.
      */
-    private OpsType mOpsInstance;
+    private PresenterType mPresenterInstance;
 
     /**
      * Initialize or reinitialize the Presenter layer.  This must be
      * called *after* the onCreate(Bundle saveInstanceState) method.
      *
      * @param opsType 
-     *            Class object that's used to create an operations
-     *            ("Ops") object.  
+     *            Class object that's used to create a Presenter object.
      * @param view
-     *            Reference to the RequiredViewOps in the View layer.
+     *            Reference to the RequiredViewOps object in the View layer.
      */
-    public void onCreate(Class<OpsType> opsType,
+    public void onCreate(Class<PresenterType> opsType,
                          RequiredViewOps view) {
         // Handle configuration-related events, including the initial
         // creation of an Activity and any subsequent runtime
@@ -77,12 +88,12 @@ public abstract class GenericActivity<RequiredViewOps,
     }
 
     /**
-     * Return the initialized ProvidedOps instance for use by the
-     * application.
+     * Return the initialized ProvidedPresenterOps instance for use by
+     * application logic in the View layer.
      */
     @SuppressWarnings("unchecked")
-    public ProvidedViewOps getPresenter() {
-        return (ProvidedViewOps) mOpsInstance;
+    public ProvidedPresenterOps getPresenter() {
+        return (ProvidedPresenterOps) mPresenterInstance;
     }
 
     /**
@@ -90,6 +101,53 @@ public abstract class GenericActivity<RequiredViewOps,
      */
     public RetainedFragmentManager getRetainedFragmentManager() {
         return mRetainedFragmentManager;
+    }
+
+    /**
+     * Initialize the GenericActivity fields.
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     */
+    private void initialize(Class<PresenterType> opsType,
+                            RequiredViewOps view)
+            throws InstantiationException, IllegalAccessException {
+        // Create the PresenterType object.
+        mPresenterInstance = opsType.newInstance();
+
+        // Put the PresenterInstance into the RetainedFragmentManager under
+        // the simple name.
+        mRetainedFragmentManager.put(opsType.getSimpleName(),
+                                     mPresenterInstance);
+
+        // Perform the first initialization.
+        mPresenterInstance.onCreate(view);
+    }
+
+    /**
+     * Reinitialize the GenericActivity fields after a runtime
+     * configuration change.
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     */
+    private void reinitialize(Class<PresenterType> opsType,
+                              RequiredViewOps view)
+            throws InstantiationException, IllegalAccessException {
+        // Try to obtain the PresenterType instance from the
+        // RetainedFragmentManager.
+        mPresenterInstance =
+            mRetainedFragmentManager.get(opsType.getSimpleName());
+
+        // This check shouldn't be necessary under normal
+        // circumstances, but it's better to lose state than to
+        // crash!
+        if (mPresenterInstance == null) 
+            // Initialize the GenericActivity fields.
+            initialize(opsType,
+                       view);
+        else
+            // Inform it that the runtime configuration change has
+            // completed.
+            mPresenterInstance.onConfigurationChange(view);
     }
 
     /**
@@ -107,52 +165,4 @@ public abstract class GenericActivity<RequiredViewOps,
     public Context getApplicationContext() {
         return super.getApplicationContext();
     }
-
-    /**
-     * Initialize the GenericActivity fields.
-     * @throws IllegalAccessException 
-     * @throws InstantiationException 
-     */
-    private void initialize(Class<OpsType> opsType,
-                            RequiredViewOps view)
-            throws InstantiationException, IllegalAccessException {
-        // Create the OpsType object.
-        mOpsInstance = opsType.newInstance();
-
-        // Put the OpsInstance into the RetainedFragmentManager under
-        // the simple name.
-        mRetainedFragmentManager.put(opsType.getSimpleName(),
-                                     mOpsInstance);
-
-        // Perform the first initialization.
-        mOpsInstance.onCreate(view);
-    }
-
-    /**
-     * Reinitialize the GenericActivity fields after a runtime
-     * configuration change.
-     * @throws IllegalAccessException 
-     * @throws InstantiationException 
-     */
-    private void reinitialize(Class<OpsType> opsType,
-                              RequiredViewOps view)
-            throws InstantiationException, IllegalAccessException {
-        // Try to obtain the OpsType instance from the
-        // RetainedFragmentManager.
-        mOpsInstance =
-            mRetainedFragmentManager.get(opsType.getSimpleName());
-
-        // This check shouldn't be necessary under normal
-        // circumstances, but it's better to lose state than to
-        // crash!
-        if (mOpsInstance == null) 
-            // Initialize the GenericActivity fields.
-            initialize(opsType,
-                       view);
-        else
-            // Inform it that the runtime configuration change has
-            // completed.
-            mOpsInstance.onConfigurationChange(view);
-    }
 }
-
