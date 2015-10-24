779fef4988c88a6eb5e5ab3babd485b707822dd7
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2015-10-24 09:35:15 -0500

    Updates.

--- assignments/assignment3/src/vandy/mooc/presenter/ImagePresenter.java
+++ assignments/assignment3/src/vandy/mooc/presenter/ImagePresenter.java
@@ -138,7 +138,7 @@ public class ImagePresenter
     /**
      * Hook method called to shutdown the Presenter layer.
      *
-     * @param isChangeConfigurations
+     * @param isChangingConfigurations
      *        True if a runtime configuration triggered the onDestroy() call.
      */
     @Override
