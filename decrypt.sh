6d958c728c105ebc9d742f091647b2d17d41d8bf
antonmry <ayuste@optaresolutions.com>
2015-10-24 12:17:04 +0200

    Starting issue #4

--- decrypt.sh
+++ decrypt.sh
@@ -3,9 +3,9 @@
 echo "Please enter the password";
 read -s password;
 
-cp assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/model/PalantiriManager.java assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/model/PalantiriManager.java.original
+#cp assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/model/PalantiriManager.java assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/model/PalantiriManager.java.original
 
-cp assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/presenter/PalantiriPresenter.java assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/presenter/PalantiriPresenter.java.original
+#cp assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/presenter/PalantiriPresenter.java assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/presenter/PalantiriPresenter.java.original
 
 openssl aes-128-cbc -d -salt -in assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/model/PalantiriManager.java.aes -out assignments/assignment1/A1-Android-App/app/src/main/java/edu/vandy/model/PalantiriManager.java -k $password
 
